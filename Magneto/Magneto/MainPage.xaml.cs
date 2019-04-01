a80875d56fafa5779727e21d49fb370b010c2f31
invalidcorgi <invalidcorgi@gmail.com>
2019-04-01 22:02:05 +0200

    Magneto done

--- Magneto/Magneto/MainPage.xaml.cs
+++ Magneto/Magneto/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Magneto
@@ -12,6 +13,18 @@ namespace Magneto
         public MainPage()
         {
             InitializeComponent();
+            Magnetometer.Start(SensorSpeed.UI);
+            Magnetometer.ReadingChanged += (s, e) =>
+            {
+                x.Text = e.Reading.MagneticField.X.ToString();
+                y.Text = e.Reading.MagneticField.Y.ToString();
+                z.Text = e.Reading.MagneticField.Z.ToString();
+                var absVal = Convert.ToInt32(Math.Round(e.Reading.MagneticField.Length()));
+                abs.Text = absVal.ToString();
+                this.BackgroundColor = Color.FromRgb(255 - (absVal <= 510 ? absVal / 2 : 255), 0 + (absVal <= 510 ? absVal / 2 : 255), 0);
+                if(absVal >= 300)
+                    Vibration.Vibrate(50);
+            };
         }
     }
 }
